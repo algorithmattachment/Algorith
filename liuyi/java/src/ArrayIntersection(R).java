feef035ae824b9230159a845cca4be4811c6d8ce
ly <798825105@qq.com>
2019-07-08 19:20:46 +0800

    更新代码

--- liuyi/java/src/ArrayIntersection(R).java
+++ liuyi/java/src/ArrayIntersection(R).java
@@ -7,13 +7,14 @@ public class ArrayIntersection {
     public int[] intersection(int[] num1, int num2) {
         Set<Integer> arr1 = new HashSet<Integer>();
         Set<Integer> arr2 = new HashSet<Integer>();
-
+        //将数组元素存放到Set中，顺便去掉重复元素
         for (Integer num : num1)
             arr1.add(num);
         for (Integer num : arr2)
             arr2.add(num);
-
+        //两集合交集
         arr1.retainAll(arr2);
+        //遍历交集，转移结果
         int[] res = new int[arr1.size()];
         int index = 0;
         for (int num : arr1) {
